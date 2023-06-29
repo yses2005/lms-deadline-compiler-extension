c11fad9c98f9a52d36be1a7cd4dbcd913f6abecc
mynaka <avillanueva@up.edu.ph>
2023-06-29 18:55:07 +0800

    feat: added basic calenday layout

--- background.js
+++ background.js
@@ -1,3 +1,3 @@
 chrome.action.onClicked.addListener(function() {
-  chrome.tabs.create({url: 'index.html'});
+  chrome.tabs.create({'url': chrome.extension.getURL('notes.html')}, function(tab));
 });
